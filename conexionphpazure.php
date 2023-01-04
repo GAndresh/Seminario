6e36cd5c7d7629fd885fe26a5bc016633aec591e
GAndresh <andresh.app@gmail.com>
2023-01-04 18:02:02 -0500

    cambiocadena

--- conexionphpazure.php
+++ conexionphpazure.php
@@ -1,6 +1,5 @@
 <?php
 $con=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-mysqli_real_connect($con, "dbseminario.mysql.database.azure.com", "administrador@dbseminario", "Gabriellah26", "seminario2023", 3306,
-MYSQLI_CLIENTE_SSL) or die ("Error al conectar" . mysqli_error());
+mysqli_ssl_set($con, NULL, NULL, "./Seminario/SSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
+mysqli_real_connect($con, "dbseminario.mysql.database.azure.com", "administrador@dbseminario", "Gabriellah26", "seminario2023", 3306);
 ?>
