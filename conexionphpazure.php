d995db9a6229a36a551d71365d71e251fab70410
GAndresh <andresh.app@gmail.com>
2023-01-04 22:39:05 -0500

    cambio de cadena de conexion

--- conexionphpazure.php
+++ conexionphpazure.php
@@ -1,5 +1,7 @@
 <?php
-$con=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, "./Seminario/SSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-mysqli_real_connect($con, "dbseminario.mysql.database.azure.com", "administrador@dbseminario", "Gabriellah26", "seminario2023", 3306);
+$conexion=mysqli_init();
+mysqli_ssl_set($conexion, NULL, NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_real_connect($conexion, "dbseminario.mysql.database.azure.com",
+"administrador@dbseminario", "Gabriellah26", "seminario2023", 3306, 
+MYSQLI_CLIENT_SSL) or die ("error al conectar " . mysqli_error());
 ?>
